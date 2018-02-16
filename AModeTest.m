f95f350d0841df1fc36a340ed485ceab761268b9
Jonathan - M0ZJO <m0zjo-code@users.noreply.github.com>
2018-02-16 20:40:20 +0000

    Added shading

--- AModeTest.m
+++ AModeTest.m
@@ -1,19 +1,24 @@
 clear all
 N = 1000;
+Shift = N/5;
 x = linspace(0, N, N);
-var = heaviside(x-N/2);
+var = heaviside(x-Shift);
 noise = rand(1, N)/10;
 sine = sin(x)/20;
 
-sig = noise+sin+var;
+sig = noise+sine+var;
 
 actual_val = ones(1, N)+mean(noise);
-mean_val = mean(sig);
+mean_val = ones(1, N)*mean(sig);
 
 hold on
 plot(sig);
 plot(mean_val);
 plot(actual_val);
-legend("Data Signal", "Mean Signal", "Mean Mode Signal")
+ha = area([Shift N], [10 10], 'LineStyle', 'none');
+set(ha, 'FaceColor', 'r');
+alpha(0.15);
+axis([0 1000 -0.2 1.2])
+legend("Data Signal", "Mean Signal", "MeanMode Signal", "Mode Area", "Location", "best")
 xlabel("Sample Number")
 ylabel("Sensor Value (Arb. Units)")
